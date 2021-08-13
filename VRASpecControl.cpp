bacc986955f375e12f15c9c3f756f1e347065eaa
Wenzel Reichmuth <wenzel.reichmuth@vraktion.com>
2021-08-13 12:44:42 +0200

    SavLay Filter + FloatingAverage integriert
    grundlegend überarbeitet
    
    PGA Verstärkung deaktiviert

--- VRASpecControl.cpp
+++ VRASpecControl.cpp
@@ -1,114 +1,360 @@
 #include "VRASpecControl.h"
 
-VRASpecControl::VRASpecControl(UUID *p_uuid, EventQueue *p_eq, StateChain *p_stateChain, I2C *p_i2c):
-BLEService("specCtrl", p_uuid, p_eq, p_stateChain),
-eq(p_eq),
-// storage(p_storage),
-// settings(p_settings),
-i2c(p_i2c)
+VRASpecControl::VRASpecControl(
+    UUID *p_uuid,
+    EventQueue *p_eq,
+    StateChain *p_stateChain,
+    I2C *p_i2c,
+    VRAStorage *storage,
+    VRAEnvironmentControl *envCtl,
+    VRATvocControl *tvocCtl) : BLEService("specCtrl", p_uuid, p_eq, p_stateChain),
+                               eq(p_eq),
+                               // storage(p_storage),
+                               // settings(p_settings),
+                               i2c(p_i2c),
+                               storage(storage),
+                               envCtl(envCtl),
+                               tvocCtl(tvocCtl)
 {
-    this->adc = new ADS1115(this->i2c, 0x49);
+    this->interval = new IntervalEvent(this->eq, 10000, callback(this, &VRASpecControl::getAdc));
+    this->adc0 = new ADS1115(this->i2c, 0x49); //AIN0/AIN1: O3, AIN2/AIN3: NO2
+    this->adc1 = new ADS1115(this->i2c, 0x48); //AIN0/AIN1: CO
+
+    this->adc0Int = new InterruptIn(INT_ADC0, PullUp);
+    this->adc0Int->fall(callback(this, &VRASpecControl::adc0FallISR));
+    this->adc0Int->disable_irq();
+
+    this->adc1Int = new InterruptIn(INT_ADC1, PullUp);
+    this->adc1Int->fall(callback(this, &VRASpecControl::adc1FallISR));
+    this->adc1Int->disable_irq();
+
+    this->favO3 = new FloatingAverage(6);
+    this->favCO = new FloatingAverage(6);
+    this->favNO2 = new FloatingAverage(6);
+
+    this->sgO3 = new SavLayFilter(&(this->vO3), 0, 25);   //9);
+    this->sgCO = new SavLayFilter(&(this->vCO), 0, 25);   //9);
+    this->sgNO2 = new SavLayFilter(&(this->vNO2), 0, 25); //9);
 }
 
-void VRASpecControl::init(){
+void VRASpecControl::init()
+{
     printf("[specCtrl] init\r\n");
-    this->conversationDelay = this->adc->calcConversationDelay(this->dataRate)/1000;
+    this->adc0->setDataRate(this->dataRate);
+    this->adc0->setVoltageRange(this->vrO3);
+    this->adc0->enableConvReadyPin(1);
+
+    this->adc1->setDataRate(this->dataRate);
+    this->adc1->setVoltageRange(this->vrCO);
+    this->adc1->enableConvReadyPin(1);
+
+    this->adc0Int->enable_irq();
+    this->adc1Int->enable_irq();
 }
 
-void VRASpecControl::initCharacteristics(){
+void VRASpecControl::adc0FallISR()
+{
+    this->eq->call(callback(this, &VRASpecControl::adc0Ready));
+}
+
+void VRASpecControl::adc1FallISR()
+{
+    this->eq->call(callback(this, &VRASpecControl::adc1Ready));
+}
+
+void VRASpecControl::initCharacteristics()
+{
     printf("[specCtrl] init Characteristics\r\n");
+    // this->addCharacteristic(
+    //     new BLENotifyCharacteristic(
+    //         (uint16_t)VRASpecControl::Characteristics::Spec,
+    //         12, //size
+    //         this->eq,
+    //         10000,  //interval
+    //         10000,  //min
+    //         600000, //max
+    //         callback(this, &VRASpecControl::getAdc)));
+
     this->addCharacteristic(
-        new BLENotifyCharacteristic(
-            (uint16_t) VRASpecControl::Characteristics::Spec,
-            12,//size
-            this->eq,
-            1000,//interval
-            1000,//min
-            600000,//max
-            callback(this, &VRASpecControl::getAdc)
-        )
-    );
+        new BLECharacteristic(
+            (uint16_t)VRASpecControl::Characteristics::Spec,
+            GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY,
+            12));
+
+    this->addCharacteristic(
+        new BLECharacteristic(
+            (uint16_t)VRASpecControl::Characteristics::RawSpec,
+            GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY,
+            12));
+
+    this->addCharacteristic(
+        new BLECharacteristic(
+            (uint16_t)VRASpecControl::Characteristics::ZeroVoltage,
+            GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY,
+            12));
+
+    this->addCharacteristic(
+        new BLECharacteristic(
+            (uint16_t)VRASpecControl::Characteristics::AvgSpec,
+            GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY,
+            12));
 }
 
-void VRASpecControl::pastBleInit(){
+void VRASpecControl::pastBleInit()
+{
     printf("[specCtrl] pastBleInit\r\n");
+
+    float zeroVoltages[3];
+    this->storage->get("zero", (void *)zeroVoltages, 12);
+
+    if (zeroVoltages[0] > 0 && zeroVoltages[0] < 0.00004)
+    {
+        this->zeroCO = zeroVoltages[0];
+        printf("[specCtrl] using CO zero from storage %d\r\n", (int)(this->zeroCO * 1E6));
+    }
+    else
+    {
+        zeroVoltages[0] = this->zeroCO;
+    }
+
+    if (zeroVoltages[1] > 0.04 && zeroVoltages[1] < 0.05)
+    {
+        this->zeroNO2 = zeroVoltages[1];
+        printf("[specCtrl] using NO2 zero from storage %d\r\n", (int)(this->zeroNO2 * 1E6));
+    }
+    else
+    {
+        zeroVoltages[1] = this->zeroNO2;
+    }
+
+    if (zeroVoltages[2] > 0.007 && zeroVoltages[2] < 0.008)
+    {
+        this->zeroO3 = zeroVoltages[2];
+        printf("[specCtrl] using O3 zero from storage %d\r\n", (int)(this->zeroO3 * 1E6));
+    }
+    else
+    {
+        zeroVoltages[2] = this->zeroO3;
+    }
+
+    this->favCO->flush(this->zeroCO);
+    this->favNO2->flush(this->zeroNO2);
+    this->favO3->flush(this->zeroO3);
+
+    this->setGatt((uint16_t)VRASpecControl::Characteristics::Spec, zeroVoltages, 3);
+    this->setGatt((uint16_t)VRASpecControl::Characteristics::RawSpec, zeroVoltages, 3);
+    this->setGatt((uint16_t)VRASpecControl::Characteristics::ZeroVoltage, zeroVoltages, 3);
+    this->setGatt((uint16_t)VRASpecControl::Characteristics::AvgSpec, zeroVoltages, 3);
+
+    this->interval->start();
 }
 
-void VRASpecControl::onStateOff(){
+void VRASpecControl::onStateOff()
+{
     printf("[specCtrl] off\r\n");
 }
 
-void VRASpecControl::onStateStandby(){
+void VRASpecControl::onStateStandby()
+{
     printf("[specCtrl] standby\r\n");
 }
 
-void VRASpecControl::onStateOn(){
+void VRASpecControl::onStateOn()
+{
     printf("[specCtrl] on\r\n");
 }
 
-void VRASpecControl::getAdc(){
+void VRASpecControl::getAdc()
+{
     printf("[specCtrl] get adc\r\n");
-    int delay = this->conversationDelay + 50;
-    this->eq->call(callback(this, &VRASpecControl::startSpecSensor), VRASpecControl::SpecSensors::CO);
-    this->eq->call_in(delay, callback(this, &VRASpecControl::startSpecSensor), VRASpecControl::SpecSensors::NO2);
-    this->eq->call_in(2 * delay, callback(this, &VRASpecControl::startSpecSensor), VRASpecControl::SpecSensors::O3);
+
+    //this->adc0->setVoltageRange(this->vrO3);//VR
+    this->adc0->startConversation(chan_0_1);
+    this->adc1->startConversation(chan_0_1);
 }
 
-void VRASpecControl::startSpecSensor(VRASpecControl::SpecSensors spec){
-    double val = 0;
-    for(int i = 0; i< this->averageSize; i++){
-        this->adc->startConversation((chan_t) spec, this->voltageRange, this->dataRate);
-        wait_us(this->adc->getConversationDelay()+2000);
-        val += (double) this->adc->getLastConversionResults_V(this->voltageRange);
-    } 
-    switch(spec){
-        case VRASpecControl::SpecSensors::CO://first
-            this->vCO = (float)(val / this->averageSize);
-            printf("CO: %d\r\n", (int)(this->vCO*1E4));
-        break;
-        case VRASpecControl::SpecSensors::NO2://second
-            this->vNO2 = (float)(val / this->averageSize);
-            printf("NO2: %d\r\n", (int)(this->vNO2*1E4));
-        break;
-        case VRASpecControl::SpecSensors::O3://last
-            this->vO3 = (float)(val / this->averageSize);
-            float spec[3]{
-                this->vCO,
-                this->vNO2,
-                this->vO3
-            };
-            this->setGatt((uint16_t) VRASpecControl::Characteristics::Spec, spec, 3);
-            printf("O3: %d\r\n", (int)(this->vO3*1E4));
-        break;
-        // default:
-        // break;
+void VRASpecControl::getAdc2()
+{
+    // this->adc0->setVoltageRange(this->vrNO2);//VR
+    this->adc0->startConversation(chan_2_3);
+}
+
+void VRASpecControl::adc0Ready()
+{
+    float val = this->adc0->getLastConversionResults_V();
+
+    // adsVR_t vr = this->adc0->setVoltageRangeByVal(val);//VR
+
+    // printf(">> [%d] %d adc0 ready %d\r\n", this->convCnt0, (uint32_t)time(NULL), (int)(val * 1E5));
+    this->convCnt0++;
+    if (this->convCnt0 <= this->maxConv)
+    {
+
+        this->vO3sum += (double)val;
+        // printf("vr O3 %04X\r\n", vr);
+
+        // printf("O3 %d %d\r\n", this->testCnt, (int)(val * 1E5));
+        // this->testCnt++;
+
+        if (this->convCnt0 < this->maxConv)
+        {
+            // this->vrO3 = vr;//VR
+            this->adc0->startConversation(chan_0_1);
+        }
+        else //switch condition
+        {
+            this->eq->call_in(1000, callback(this, &VRASpecControl::getAdc2)); //delay next channel
+            // this->adc0->setVoltageRange(this->vrNO2);
+            // this->adc0->startConversation(chan_2_3);
+        }
     }
+    else if (this->convCnt0 <= 2 * this->maxConv)
+    {
+        this->vNO2sum += (double)val;
+        // printf("vr NO2 %04X\r\n", vr);
+        // printf("NO2 %d %d\r\n", this->testCnt, (int)(val * 1E5));
+        // this->testCnt++;
 
-            // int delay = this->conversationDelay + 2;
-        // this->eq->call_in(delay, callback(this, &VRASpecControl::readSpecSensor), spec);
+        if (this->convCnt0 < 2 * this->maxConv)
+        {
+            this->adc0->startConversation(chan_2_3);
+            // this->vrNO2 = vr;//VR
+        }
+        else //stop condition
+        {
+            // printf("STOP COND\r\n");
+            // this->adc0->setVoltageRange(this->vrO3);//VR
+            this->testCnt = 0;
+
+            this->vO3 = this->vO3sum / (double)this->maxConv;
+            this->vO3sum = 0;
+
+            this->vNO2 = this->vNO2sum / (double)this->maxConv;
+            this->vNO2sum = 0;
+
+            this->convCnt0 = 0;
+
+            this->eq->call(callback(this, &VRASpecControl::writeSpecToGatt));
+        }
+    }
+    else
+    {
+    }
 }
 
-void VRASpecControl::readSpecSensor(VRASpecControl::SpecSensors spec){
-    switch(spec){
-        case VRASpecControl::SpecSensors::CO://first
-            this->vCO = this->adc->getLastConversionResults_V(this->voltageRange);
-            printf("CO: %d\r\n", (int)(this->vCO*1E4));
-        break;
-        case VRASpecControl::SpecSensors::NO2://second
-            this->vNO2 = this->adc->getLastConversionResults_V(this->voltageRange);
-            printf("NO2: %d\r\n", (int)(this->vNO2*1E4));
-        break;
-        case VRASpecControl::SpecSensors::O3://last
-            this->vO3 = this->adc->getLastConversionResults_V(this->voltageRange);
-            float spec[3]{
-                this->vCO,
-                this->vNO2,
-                this->vO3
-            };
-            this->setGatt((uint16_t) VRASpecControl::Characteristics::Spec, spec, 3);
-            printf("O3: %d\r\n", (int)(this->vO3*1E4));
-        break;
-        // default:
-        // break;
+void VRASpecControl::adc1Ready()
+{
+    float val = this->adc1->getLastConversionResults_V();
+    // adsVR_t vr = this->adc1->setVoltageRangeByVal(val);//VR
+
+    // printf(">> [%d] %d adc1 ready %d\r\n", this->convCnt1, (uint32_t)time(NULL), (int)(val * 1E5));
+    this->convCnt1++;
+    if (this->convCnt1 < this->maxConv)
+    {
+        // printf("vr CO %04X\r\n", vr);
+        this->vCOsum += (double)val;
+        this->adc1->startConversation(chan_0_1);
+        // this->vrCO = vr;//VR
+    }
+    else
+    {
+        this->vCO = this->vCOsum / (double)this->maxConv;
+        this->vCOsum = 0;
+
+        this->convCnt1 = 0;
+    }
+}
+
+void VRASpecControl::writeSpecToGatt()
+{
+    printf("V CO %d\r\n", (int)(this->vCO * 1E6));
+    printf("V NO2 %d\r\n", (int)(this->vNO2 * 1E6));
+    printf("V O3 %d\r\n", (int)(this->vO3 * 1E6));
+
+    float rawSpec[3]{
+        (float)this->vCO,
+        (float)this->vNO2,
+        (float)this->vO3};
+
+    this->setGatt((uint16_t)VRASpecControl::Characteristics::RawSpec, rawSpec, 3);
+
+    float smoothCO = (float)this->sgCO->Compute();
+    float smoothNO2 = (float)this->sgNO2->Compute();
+    float smoothO3 = (float)this->sgO3->Compute();
+
+    float spec[3]{
+        smoothCO,
+        smoothNO2,
+        smoothO3};
+
+    printf("sV CO %d\r\n", (int)(smoothCO * 1E6));
+    printf("sV NO2 %d\r\n", (int)(smoothNO2 * 1E6));
+    printf("sV O3 %d\r\n", (int)(smoothO3 * 1E6));
+
+    if ((uint32_t)time(NULL) > 600) // ggf um 120sec erhoehen
+    {
+        this->favCO->add(smoothCO);
+        this->favNO2->add(smoothNO2);
+        this->favO3->add(smoothO3);
+
+        float avgCO = this->favCO->get();
+        float avgNO2 = this->favNO2->get();
+        float avgO3 = this->favO3->get();
+
+        float avgSpec[3]{
+            avgCO,
+            avgNO2,
+            avgO3};
+
+        printf("aV CO %d\r\n", (int)(avgCO * 1E6));
+        printf("aV NO2 %d\r\n", (int)(avgNO2 * 1E6));
+        printf("aV O3 %d\r\n", (int)(avgO3 * 1E6));
+
+        this->setGatt((uint16_t)VRASpecControl::Characteristics::Spec, spec, 3);
+        this->setGatt((uint16_t)VRASpecControl::Characteristics::AvgSpec, avgSpec, 3);
+        this->eq->call(callback(this, &VRASpecControl::checkZeroVoltages), avgCO, avgNO2, avgO3);
+    }
+}
+
+void VRASpecControl::checkZeroVoltages(float vCO, float vNO2, float vO3)
+{
+    uint32_t time_s = (uint32_t)time(NULL);
+    float temperature = this->envCtl->getTemperature();
+    float airQuality = this->tvocCtl->getTvoc();
+
+    printf("[specCtrl] zero voltage check. time %d temperature %d airQuali %d\r\n", time_s, (int)(temperature), (int)(airQuality));
+
+    if (time_s > 600 && temperature > 17.0 && temperature < 23.0 && airQuality < 3.0) //grundvoraussetzung
+    {
+        bool change = false;
+        if (vCO > 0 && vCO < this->zeroCO)
+        {
+            change = true;
+            this->zeroCO = vCO;
+            printf("[specCtrl] new CO zero voltage: %d\r\n", (int)(this->zeroCO * 1E6));
+        }
+        if (vNO2 > 0.04 && vNO2 < this->zeroNO2)
+        {
+            change = true;
+            this->zeroNO2 = vNO2;
+            printf("[specCtrl] new NO2 zero voltage: %d\r\n", (int)(this->zeroNO2 * 1E6));
+        }
+        if (vO3 > 0.007 && vO3 < this->zeroO3)
+        {
+            change = true;
+            this->zeroO3 = vO3;
+            printf("[specCtrl] new O3 zero voltage: %d\r\n", (int)(this->zeroO3 * 1E6));
+        }
+
+        if (change)
+        {
+            printf("[specCtrl] post new zero voltage\r\n");
+            float zeroVoltages[3]{
+                this->zeroCO,
+                this->zeroNO2,
+                this->zeroO3};
+            this->setGatt((uint16_t)VRASpecControl::Characteristics::ZeroVoltage, zeroVoltages, 3);
+            this->storage->set("zero", (void *)zeroVoltages, 12);
+        }
     }
 }
--- VRASpecControl.h
+++ VRASpecControl.h
@@ -3,62 +3,110 @@
 
 #include "BLEService.h"
 
+#include "VRAEnvironmentControl.h"
+#include "VRATvocControl.h"
+#include "VRAStorage.h"
+
 // #include "VRASettings.h"
 // #include "VRAStorage.h"
 #include "IntervalEvent.h"
 #include "BLENotifyCharacteristic.h"
+#include "FloatingAverage.h"
+#include "SavLayFilter.h"
 
 #include "ADS1x15.h"
+#include "mbed.h"
 
 class VRASpecControl : public BLEService
 {
 public:
-    enum class Characteristics: uint16_t
+    enum class Characteristics : uint16_t
     {
-        Spec = 0xFF00
+        Spec = 0xFF00,
+        RawSpec = 0xFF01,
+        ZeroVoltage = 0xFF02,
+        AvgSpec = 0xFF03
     };
 
-    enum class SpecSensors
-    {
-        CO = chan_0,
-        NO2 = chan_1,
-        O3 = chan_2
-    };
-
-    VRASpecControl(UUID *p_uuid, EventQueue *p_eq, StateChain *p_stateChain, I2C *p_i2c);
+    VRASpecControl(UUID *p_uuid, EventQueue *p_eq, StateChain *p_stateChain, I2C *p_i2c, VRAStorage *storage, VRAEnvironmentControl *envCtl, VRATvocControl *tvocCtl);
 
     void init();
     void initCharacteristics();
     void pastBleInit();
 
-    void startSpecSensor(SpecSensors spec);
-    void readSpecSensor(SpecSensors spec);
-    
-private:
+    void adc0RiseISR();
+    void adc1RiseISR();
 
+    void adc0FallISR();
+    void adc1FallISR();
+
+private:
     void onStateOff();
     void onStateStandby();
     void onStateOn();
 
     void getAdc();
+    void getAdc2();
+
+    void writeSpecToGatt();
+
+    void adc0Ready();
+    void adc1Ready();
+
+    void checkZeroVoltages(float vCO, float vNO2, float vO3);
 
     EventQueue *eq;
+    IntervalEvent *interval;
     // VRASettings *settings;
     // VRAStorage *storage;
 
     I2C *i2c;
-    ADS1115 *adc;
+    ADS1115 *adc0;
+    ADS1115 *adc1;
 
-    const adsVR_t voltageRange = VR_p_m_4_096V;
-    const adsDR_t dataRate = ADS1115_DR_860SPS;//ADS1115_DR_32SPS;//ADS1115_DR_8SPS;
+    InterruptIn *adc0Int;
+    InterruptIn *adc1Int;
+
+    adsVR_t vrO3{VR_p_m_4_096V};
+    adsVR_t vrNO2{VR_p_m_4_096V};
+    adsVR_t vrCO{VR_p_m_4_096V};
+    static const adsDR_t dataRate{ADS1115_DR_8SPS}; //averaging inchip ADS1115_DR_8SPS
+
     int conversationDelay{0};
 
-    float vO3{.0};
-    float vCO{.0};
-    float vNO2{.0};
+    double vO3{.5};
+    double vCO{.5};
+    double vNO2{.5};
 
-    static const int averageSize{15};
-    int avgCnt{0};
+    FloatingAverage *favO3;
+    FloatingAverage *favCO;
+    FloatingAverage *favNO2;
+
+    SavLayFilter *sgO3;
+    SavLayFilter *sgCO;
+    SavLayFilter *sgNO2;
+
+    bool voltageRangeUpdated = false;
+
+    const int maxConv{20};
+    volatile int convCnt0{0};
+    volatile int convCnt1{0};
+
+    double vO3sum{.0};
+    double vCOsum{.0};
+    double vNO2sum{.0};
+
+    float zeroCO{0.00004};
+    float zeroNO2{0.05};
+    float zeroO3{0.008};
+
+    int testCnt{0};
+
+    volatile bool firstHit{false};
+
+    VRAEnvironmentControl *envCtl;
+    VRATvocControl *tvocCtl;
+    VRAStorage *storage;
 };
 
 #endif //
